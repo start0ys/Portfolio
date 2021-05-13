1084bd9ecf317862ea2dd69d27373f2740af725b
시작 <78341551+start0ys@users.noreply.github.com>
2021-05-13 14:26:48 +0900

    대략적완성 내용만 추가하면될거같다

--- m_wheel.js
+++ m_wheel.js
@@ -47,3 +47,103 @@ function mouse_move(num) {
     }
 
 }
+
+
+var imgCase = 1;
+
+setInterval(function () {
+
+    var date = new Date();
+    var seconds = date.getSeconds();
+    if (imgCase == 1){
+        if (seconds % 9 == 1) {
+            img(1);
+        } else if (seconds % 9 == 2) {
+            img(2);
+        } else if (seconds % 9 == 3) {
+            img(3);
+        } else if (seconds % 9 == 4) {
+            img(4);
+        } else if (seconds % 9 == 5) {
+            img(5);
+        } else if (seconds % 9 == 6) {
+            img(6);
+        } else if (seconds % 9 == 7) {
+            img(7);
+        } else if (seconds % 9 == 8) {
+            img(8);
+        } else if (seconds % 9 == 0) {
+            img(9);
+        }
+    }
+    else if (imgCase == 2){
+        if (seconds % 14 == 1) {
+            img(1);
+        } else if (seconds % 14 == 2) {
+            img(2);
+        } else if (seconds % 14 == 3) {
+            img(3);
+        } else if (seconds % 14 == 4) {
+            img(4);
+        } else if (seconds % 14 == 5) {
+            img(5);
+        } else if (seconds % 14 == 6) {
+            img(6);
+        } else if (seconds % 14 == 7) {
+            img(7);
+        } else if (seconds % 14 == 8) {
+            img(8);
+        } else if (seconds % 14 == 9) {
+            img(9);
+        } else if (seconds % 14 == 10) {
+            img(10);
+        } else if (seconds % 14 == 11) {
+            img(11);
+        } else if (seconds % 14 == 12) {
+            img(12);
+        } else if (seconds % 14 == 13) {
+            img(13);
+        } else if (seconds % 14 == 0) {
+            img(14);
+        }
+    }
+}, 1000);
+
+
+
+function img(num) {
+    var img = document.getElementById("pjPhoto");
+    img.setAttribute("src", "img/"+imgCase+"/" + num + ".jpg");
+    return num += 1;
+
+}
+
+function nextImg(idx){
+    if (idx) {
+        if (imgCase == 2) return;
+        imgCase++;
+    } else {
+        if (imgCase == 1) return;
+        imgCase--;
+    }
+    
+    nextExplain(imgCase);
+
+    var photoCount = document.getElementById("photoCount");
+    photoCount.innerText = "(" + imgCase + "/2)";
+
+}
+
+function nextExplain(num){
+    var cv = document.getElementById("cv");
+    var pjbl = document.getElementById("pjbl");
+
+    if(num == 1){
+        cv.style.display = "block";
+        pjbl.style.display = "none";
+    } else if (num == 2){
+        cv.style.display = "none";
+        pjbl.style.display = "block";
+    }
+
+}
